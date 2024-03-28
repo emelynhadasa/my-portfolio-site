4b4d3bffc7feedf742439008436d6111c898f10a
Emelyn Hadasa <emelyndhadasa@gmail.com>
2024-03-29 01:39:39 +0700

    Add mailto

--- src/components/ConfessionBox.jsx
+++ src/components/ConfessionBox.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap' 
+import { Button } from 'react-bootstrap'
 
 const ConfessionBox = () => {
   const [message, setMessage] = useState('')
@@ -8,44 +8,31 @@ const ConfessionBox = () => {
     setMessage(e.target.value)
   }
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    try {
-      const response = await fetch('/api/confessions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message }),
-      })
-      if (response.ok) {
-        console.log('You have successfully sent it to Emelyn!')
-      } else {
-        console.error('Nuh-uh, your message failed to be delivered!')
-      }
-    } catch (error) {
-      console.error('Error submitting message:', error)
-    }
+    const mailtoLink = `mailto:emelyndhadasa@gmail.com?subject=Hey, I'm from your portfolio website ;)&body=${encodeURIComponent(message)}`
+    window.location.href = mailtoLink
   }
 
   return (
     <div className="confession-box">
       <h2>You might want to confess something...</h2>
-      <p>Don’t worry! Your message to Emelyn won’t be seen <br/> by other site visitors. <img src="../../public/images/wink2.png" className="wink-icon"/></p>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formConfession">
-          <Form.Control
-            as="textarea"
-            rows={3}
-            value={message}
-            onChange={handleMessageChange}
-            placeholder="E.g. Would you be happy to collaborate with me ..."
-            className='confession-form'
-            style={{ fontFamily: 'var(--inter)', textAlign: 'left', paddingLeft: '1rem' }}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" className='confession-btn'>Send</Button>
-      </Form>
+      <p>
+        Don’t worry! Your message will be sent<br /> to Em’s Google Mail. <img src="../../public/images/wink2.png" className="wink-icon" alt="Wink Icon" />
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          rows={3}
+          value={message}
+          onChange={handleMessageChange}
+          placeholder="E.g. Would you be happy to collaborate with me ..."
+          className="confession-form"
+          style={{ fontFamily: 'var(--inter)', textAlign: 'left', paddingLeft: '1rem' }}
+        />
+        <Button variant="primary" type="submit" className="confession-btn">
+          Send
+        </Button>
+      </form>
     </div>
   )
 }
